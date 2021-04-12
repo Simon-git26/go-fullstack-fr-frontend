84b90a757105a11feef839108c4fe646503c12ec
Simon Balleux <simoncestmoi@hotmail.fr>
2021-04-12 12:04:54 +0200

    Instaler le server ok apar pour nodemon

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require('http');
 
 // Crée un server grace a notre package qui prend en argument la fonction qui sera appelé par le server à chaque requette recu
 const server = http.createServer((req, res) => {
-    res.end('Voila la réponse du serveur si jai de la chance !');
+    res.end("Voila la réponse du serveur si jai de la chance et que je n'est pas fait n'importe quoi  !");
 });
 
 // Ecouter/Attendre les requetes envoyés
