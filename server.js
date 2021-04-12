09b39d1fb3e8901732d5455f97fce444dd15881a
Simon Balleux <simoncestmoi@hotmail.fr>
2021-04-12 11:54:47 +0200

    Instalations du server

---
+++ server.js
@@ -0,0 +1,14 @@
+
+//-----Pour tester/mettre en route le serveur, aller dans dossier backend et rentrer la commande node server et aller sur l'url : http://localhost:3000/ ------------
+//---------Au lieu de node server on peut faire nodemon server puisque je les installer mais la ca marche pas-------------
+
+// Importer le package http de node
+const http = require('http');
+
+// Crée un server grace a notre package qui prend en argument la fonction qui sera appelé par le server à chaque requette recu
+const server = http.createServer((req, res) => {
+    res.end('Voila la réponse du serveur si tout marche correctement et que jai un peu de chance !');
+});
+
+// Ecouter/Attendre les requetes envoyés
+server.listen(process.env.PORT || 3000);
